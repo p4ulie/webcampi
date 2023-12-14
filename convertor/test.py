f1de83acd74b24132110e9e8f0d9d34a30dd2df7
Pavol Antalík <pavol.antalik@gmail.com>
2023-12-15 00:09:02 +0100

    Update test lambda function -> download, encode, upload

--- convertor/test.py
+++ convertor/test.py
@@ -21,32 +21,48 @@ def upload_to_s3(bucket_name, local_file_path, s3_directory, s3_file_name):
 def lambda_handler(event, context):
     # Replace with your bucket name and source/destination directories
     s3_bucket_name = 'webcampi'
-    s3_base_directory = 'images/2023/12/01/'  # Specify the base directory path within the bucket
-    source_directory = '/tmp/source/'
-    destination_directory = '/tmp/output.mp4'
-    output_file_name = 'output.mp4'  # Name for the output video file
+
+    # s3_date_year = '2023'
+    s3_date_year = event.get('sourceParameters',{}).get('year','n/a')
+    # s3_date_month = '12'
+    s3_date_month = event.get('sourceParameters',{}).get('month','n/a')
+    # s3_date_day = '01'
+    s3_date_day = event.get('sourceParameters',{}).get('day','n/a')
+
+    s3_source_directory = f'{s3_date_year}/{s3_date_month}/{s3_date_day}/'
+    s3_destination_directory = 'video'
+
+    image_directory = '/tmp/images'
+    output_directory = '/tmp/output'
+    output_file_name = f'{s3_date_year}_{s3_date_month}_{s3_date_day}_test.mp4'  # Name for the output video file
 
     # Ensure the local directories exist
-    os.makedirs(source_directory, exist_ok=True)
+    os.makedirs(image_directory, exist_ok=True)
+    os.makedirs(output_directory, exist_ok=True)
 
     # List subdirectories within the base directory
     s3 = boto3.client('s3')
     s3_resource = boto3.resource('s3')
     bucket = s3_resource.Bucket(s3_bucket_name)
 
-    for obj in bucket.objects.filter(Prefix=s3_base_directory):
-        if obj.key != s3_base_directory:
-            subdirectory = obj.key[len(s3_base_directory):].split('/')[0]
-            download_from_s3(s3_bucket_name, obj.key, os.path.join(source_directory, subdirectory))
+    for obj in bucket.objects.filter(Prefix=s3_source_directory):
+        if obj.key != s3_source_directory:
+            subdirectory = obj.key[len(s3_source_directory):].split('/')[0]
+            download_from_s3(s3_bucket_name, obj.key, os.path.join(image_directory))
 
     # Change directory to access the source files
-    os.chdir(source_directory)
+    os.chdir(image_directory)
 
     # Use FFmpeg to create a video from images in the directory
-    subprocess.run(['ffmpeg', '-framerate', '30', '-pattern_type', 'glob', '-i', '*.jpg', '-c:v', 'libx265', '-pix_fmt', 'yuv420p', destination_directory], check=True)
+    subprocess.run(['ffmpeg', '-framerate', '30', '-pattern_type', 'glob', '-i', '*.jpg', '-c:v', 'libx265', '-pix_fmt', 'yuv420p', os.path.join(output_directory, output_file_name)], check=True)
 
     # Upload the resulting video to S3
-    upload_to_s3(s3_bucket_name, destination_directory, 'videos', output_file_name)
+    upload_to_s3(
+        bucket_name=s3_bucket_name,
+        local_file_path=os.path.join(output_directory, output_file_name),
+        s3_directory=s3_destination_directory,
+        s3_file_name=output_file_name
+    )
 
     return {
         'statusCode': 200,
