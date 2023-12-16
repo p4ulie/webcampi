65b42f49971cc59780c16927dcba7898444881cf
Pavol Antalík <pavol.antalik@gmail.com>
2023-12-16 18:29:55 +0100

    Remove print debug statements

--- convertor/test.py
+++ convertor/test.py
@@ -60,19 +60,16 @@ def lambda_handler(event, context):
         if obj.key != s3_source_directory:
             subdirectory = obj.key[len(s3_source_directory):].split('/')[0]
             logger.info(f'Download image {obj.key}')
-            print(f'Download image {obj.key}')
             download_from_s3(s3_bucket_name, obj.key, os.path.join(image_directory))
 
     # Change directory to access the source files
     os.chdir(image_directory)
 
     logger.info('Encode the video')
-    print('Encode the video')
     # Use FFmpeg to create a video from images in the directory
     subprocess.run(['ffmpeg', '-y', '-framerate', '30', '-pattern_type', 'glob', '-i', '*.jpg', '-c:v', 'libx265', '-pix_fmt', 'yuv420p', os.path.join(output_directory, output_file_name)], check=True)
 
     logger.info('Upload the video {os.path.join(output_directory, output_file_name)}')
-    print('Upload the video {os.path.join(output_directory, output_file_name)}')
     # Upload the resulting video to S3
     upload_to_s3(
         bucket_name=s3_bucket_name,
