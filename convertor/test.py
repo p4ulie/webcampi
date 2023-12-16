27f6d324e530253236251c4715efb3fa76c771a3
Pavol Antalík <pavol.antalik@gmail.com>
2023-12-16 17:03:44 +0100

    Pass event data to handler for testing

--- convertor/test.py
+++ convertor/test.py
@@ -3,6 +3,7 @@ import boto3
 import os
 import tempfile
 import logging
+import json
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -23,18 +24,26 @@ def upload_to_s3(bucket_name, local_file_path, s3_directory, s3_file_name):
     s3.upload_file(local_file_path, bucket_name, f"{s3_directory}/{s3_file_name}")
 
 def lambda_handler(event, context):
-    # Replace with your bucket name and source/destination directories
-    s3_bucket_name = 'webcampi'
+    s3_parameters = json.loads(event)['s3_parameters']
 
-    # s3_date_year = '2023'
-    s3_date_year = event.get('sourceParameters',{}).get('year','n/a')
-    # s3_date_month = '12'
-    s3_date_month = event.get('sourceParameters',{}).get('month','n/a')
-    # s3_date_day = '01'
-    s3_date_day = event.get('sourceParameters',{}).get('day','n/a')
+    # Replace with your bucket name and source/destination directories
+    if 'bucket_name' in s3_parameters.keys():
+        s3_bucket_name = s3_parameters['bucket_name']
+    else:
+        s3_bucket_name ='webcampi'
+
+    s3_date_year = s3_parameters['bucket_directory_year']
+    # s3_date_year = event.get('sourceParameters',{}).get('year','n/a')
+    s3_date_month = s3_parameters['bucket_directory_month']
+    # s3_date_month = event.get('sourceParameters',{}).get('month','n/a')
+    s3_date_day = s3_parameters['bucket_directory_day']
+    # s3_date_day = event.get('sourceParameters',{}).get('day','n/a')
 
     s3_source_directory = f'{s3_date_year}/{s3_date_month}/{s3_date_day}/'
-    s3_destination_directory = 'video'
+    if 'bucket_name' in s3_parameters.keys():
+        s3_destination_directory = s3_parameters['bucket_directory_destination']
+    else:
+        s3_destination_directory = 'video'
 
     image_directory = '/tmp/images'
     output_directory = '/tmp/output'
@@ -54,13 +63,14 @@ def lambda_handler(event, context):
             subdirectory = obj.key[len(s3_source_directory):].split('/')[0]
             logger.info(f'Download image {obj.key}')
             download_from_s3(s3_bucket_name, obj.key, os.path.join(image_directory))
+            break
 
     # Change directory to access the source files
     os.chdir(image_directory)
 
     logger.info('Encode the video')
     # Use FFmpeg to create a video from images in the directory
-    subprocess.run(['ffmpeg', '-framerate', '30', '-pattern_type', 'glob', '-i', '*.jpg', '-c:v', 'libx265', '-pix_fmt', 'yuv420p', os.path.join(output_directory, output_file_name)], check=True)
+    subprocess.run(['ffmpeg', '-y', '-framerate', '30', '-pattern_type', 'glob', '-i', '*.jpg', '-c:v', 'libx265', '-pix_fmt', 'yuv420p', os.path.join(output_directory, output_file_name)], check=True)
 
     logger.info('Upload the video {os.path.join(output_directory, output_file_name)}')
     # Upload the resulting video to S3
@@ -77,4 +87,11 @@ def lambda_handler(event, context):
     }
 
 if __name__ == "__main__":
-    lambda_handler("", "")
+    event = {
+        's3_parameters': {
+            'bucket_directory_year': '2023',
+            'bucket_directory_month': '12',
+            'bucket_directory_day': '01'
+        }
+    }
+    lambda_handler(json.dumps(event), "")
