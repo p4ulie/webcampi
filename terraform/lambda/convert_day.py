14f9be5780879ff9525d5b9dd32f0b03230f5ba1
pavol.antalik <pavol.antalik@gmail.com>
2024-01-04 00:36:53 +0100

    Update S3 destination directory

--- terraform/lambda/convert_day.py
+++ terraform/lambda/convert_day.py
@@ -40,6 +40,8 @@ def lambda_handler(event, context):
         if 'bucket_directory_day' in s3_parameters.keys():
             s3_date_day = s3_parameters['bucket_directory_day']
 
+        s3_destination_directory = f'video_processing/{s3_date_year}_{s3_date_month}_{s3_date_day}'
+
         if 'bucket_directory_destination' in s3_parameters.keys():
             s3_destination_directory = s3_parameters['bucket_directory_destination']
 
