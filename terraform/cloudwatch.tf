bd19c6672bfb21fac9ef3da8a6fb6f5264fef13c
pavol.antalik <pavol.antalik@gmail.com>
2024-01-27 14:11:55 +0100

    Add loggroups

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -9,3 +9,15 @@ resource "aws_cloudwatch_log_group" "convert_day" {
 
   tags = var.aws_tags
 }
+
+resource "aws_cloudwatch_log_group" "combine_videos" {
+  name = "/aws/lambda/combine_videos"
+
+  tags = var.aws_tags
+}
+
+resource "aws_cloudwatch_log_group" "generate_video_page" {
+  name = "/aws/lambda/generate_video_page"
+
+  tags = var.aws_tags
+}
