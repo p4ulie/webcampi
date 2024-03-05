68e2ec3b937471bb49f7657da4e747b553c82179
pavol.antalik <pavol.antalik@gmail.com>
2024-03-05 11:55:16 +0100

    Add robots.txt

--- terraform/s3_video.tf
+++ terraform/s3_video.tf
@@ -105,3 +105,14 @@ resource "aws_s3_bucket_logging" "video" {
   target_bucket = aws_s3_bucket.logs.id
   target_prefix = "s3_access_log/"
 }
+
+resource "aws_s3_object" "robots" {
+  bucket = aws_s3_bucket.video.id
+  key    = "robots.txt"
+  source = "robots.txt"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("robots.txt")
+}
